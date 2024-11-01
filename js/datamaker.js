8e82197109069f315e3138534b6c0933fd25ff8e
Charls <carlos.mancilla993@gmail.com>
2024-10-31 20:47:05 -0500

    planes

--- js/datamaker.js
+++ js/datamaker.js
@@ -25,3 +25,63 @@
         })
     }, 500)
 })(jQuery)
+
+function toggleActiveClass(
+    linkSelector,
+    containerSelector,
+    otherContainerSelector,
+    btnSelector
+) {
+    const link = document.querySelector(linkSelector)
+    const container = document.querySelector(containerSelector)
+    const otherContainer = document.querySelector(otherContainerSelector)
+    const registrarseBtn = document.querySelector(btnSelector)
+
+    if (link) {
+        link.addEventListener('click', () => {
+            container.classList.toggle('active')
+            otherContainer.classList.remove('active')
+
+            if (
+                container.classList.contains('active') ||
+                otherContainer.classList.contains('active')
+            ) {
+                registrarseBtn.classList.add('desactivated')
+            } else {
+                registrarseBtn.classList.remove('desactivated')
+            }
+        })
+    }
+}
+
+toggleActiveClass(
+    '.plans-link-oro',
+    '.plan__price-table-oro',
+    '.plan__price-table-platino',
+    '.registrarse-gratuito'
+)
+toggleActiveClass(
+    '.plans-link-platino',
+    '.plan__price-table-platino',
+    '.plan__price-table-oro',
+    '.registrarse-gratuito'
+)
+
+function actualizarTotalFormulario(selectorFormulario) {
+    const formulario = document.querySelector(selectorFormulario)
+
+    if (formulario) {
+        const radios = formulario.querySelectorAll('input[type="radio"]')
+        const total = formulario.querySelector('.total')
+
+        radios.forEach((radio) => {
+            radio.addEventListener('change', () => {
+                const valor = radio.value
+                total.querySelector('span').innerHTML = `${valor}`
+            })
+        })
+    }
+}
+
+actualizarTotalFormulario('.plan__price-table-oro')
+actualizarTotalFormulario('.plan__price-table-platino')
