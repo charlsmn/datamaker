3a5cf84f1a416aec0137329e167b07a64ad70516
Charls <carlos.mancilla993@gmail.com>
2024-11-01 14:53:16 -0500

    tabla excel

--- js/datamaker.js
+++ js/datamaker.js
@@ -105,3 +105,87 @@ function abrirPopup(popupSelector, btnSelector) {
 
 abrirPopup('.form-popup', '.form-popup-btn')
 abrirPopup('.video-popup', '.video-popup-btn')
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Función para actualizar los valores según el valor seleccionado
+    function updateValues(selectedValue) {
+        const values = {
+            100: {
+                minutos: 400,
+                horas: '6,67',
+                salario: '73.043',
+                plan: '40.000',
+                ahorro: '33.043',
+            },
+            300: {
+                minutos: 1200,
+                horas: '20,00',
+                salario: '219.130',
+                plan: '114.000',
+                ahorro: '105.130',
+            },
+            500: {
+                minutos: 2000,
+                horas: '33,33',
+                salario: '365.217',
+                plan: '180.000',
+                ahorro: '185.217',
+            },
+            1000: {
+                minutos: 4000,
+                horas: '66,67',
+                salario: '730.435',
+                plan: '340.000',
+                ahorro: '390.435',
+            },
+            3000: {
+                minutos: 12000,
+                horas: '200,00',
+                salario: '2.191.304',
+                plan: '960.000',
+                ahorro: '1.231.304',
+            },
+            10000: {
+                minutos: 40000,
+                horas: '666,67',
+                salario: '7.304.348',
+                plan: '3.000.000',
+                ahorro: '4.304.348',
+            },
+            20000: {
+                minutos: 80000,
+                horas: '1.333,33',
+                salario: '14.608.696',
+                plan: '4.800.000',
+                ahorro: '9.808.696',
+            },
+            50000: {
+                minutos: 200000,
+                horas: '3.333,33',
+                salario: '36.521.739',
+                plan: '10.000.000',
+                ahorro: '26.521.739',
+            },
+        }
+
+        const selectedData = values[selectedValue]
+        document.querySelector('.total-minutos').textContent =
+            selectedData.minutos
+        document.querySelector('.horas').textContent = selectedData.horas
+        document.querySelector('.total-salario').textContent =
+            selectedData.salario
+        document.querySelector('.plan-tabla').textContent = selectedData.plan
+        document.querySelector('.ahorro-tabla').textContent =
+            selectedData.ahorro
+    }
+
+    // Configuración inicial para cargar los valores de 100
+    updateValues('100')
+
+    // Agregar evento para actualizar valores cuando cambie el select
+    document
+        .getElementById('numero-facturas')
+        .addEventListener('change', function () {
+            updateValues(this.value)
+        })
+})
